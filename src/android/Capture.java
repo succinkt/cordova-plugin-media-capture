dba405b4ad315e1d56b54d6236d3ea02b657627e
AaronM <eximiousness@gmail.com>
2019-02-14 13:28:50 -0500

    Revert "."
    This reverts commit 59fa70092d8d43cf10a3dfbca7f3cbd8839cf203.

--- src/android/Capture.java
+++ src/android/Capture.java
@@ -304,8 +304,7 @@ public class Capture extends CordovaPlugin {
 
             if(Build.VERSION.SDK_INT > 7){
                 intent.putExtra("android.intent.extra.durationLimit", req.duration);
-                //intent.putExtra("android.intent.extra.videoQuality", req.quality);
-                intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 0);
+                intent.putExtra("android.intent.extra.videoQuality", req.quality);
             }
             this.cordova.startActivityForResult((CordovaPlugin) this, intent, req.requestCode);
         }
